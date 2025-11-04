cb87a68168804fba446ca67fb9525c39932099a4
AZIZ-Soufiane <AZIZ.Soufiane.Solicode@gmail.com>
2025-11-04 14:54:13 +0000

    Prototype Done

---
+++ Prototype/Prototype/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+            TagSeeder::class,
+            ArticleSeeder::class,
+            PivotArticleTagSeeder::class,
+        ]);
+    }
+}
